30faed74448fb5cccb9dc90de5cf27e92ac1f67b
jowjow22 <jonathan224santos@gmail.com>
2022-11-29 17:53:53 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import socket from "socket.io";
+
+let messages: any = [];
+
+const socketsIO = new socket.Server({
+  cors: {
+    origin: "http://localhost:8080",
+    credentials: true,
+  },
+
+  allowEIO3: true,
+});
+
+socketsIO.on("connection", (client: any) => {
+  console.log("made socket connection", client.id);
+
+  messages.map((message: any) => {
+    client.emit("message", message);
+  });
+
+  client.on("sendMessage", (message: string, name: string) => {
+    console.log("client is receiving message ", message);
+    messages.push({ message, name });
+    const data = {
+      message,
+      name,
+    };
+    socketsIO.emit("message", data);
+  });
+});
+
+const port = 8000;
+socketsIO.listen(port);
+console.log("listening on port ", port);
